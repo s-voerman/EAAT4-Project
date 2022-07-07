2673f5adb92fa1c8f02b76ce6d97d2faa8dba7db
s-voerman <107398066+s-voerman@users.noreply.github.com>
2022-07-07 12:00:50 +0200

    Update complex_spike_analysis.py

--- LTP_LTD/complex_spike_analysis.py
+++ LTP_LTD/complex_spike_analysis.py
@@ -8,23 +8,13 @@ from numpy import trapz
 from scipy.stats import levene, ttest_ind
 from scipy.stats import tstd, sem, shapiro, mannwhitneyu
 
+#Essential variables with directories of the data, metafiles, and output location, change this to whereever you save these items
 base_folder = "//storage/v/vcl15/ddata/NEUW/dezeeuw/Stijn Voerman/Paper_data"
 data_loc = os.path.join(base_folder, "Data/LTD/Data")
 meta_loc = os.path.join(base_folder, "Metadata/ltdmeta_select_strict.xlsx")
 output_loc = os.path.join(base_folder, "Analysis_tools/output/")
-plt.style.use('seaborn-paper')
-sns.set_palette('Set1',8,0.7)
-plt.rcParams['font.sans-serif'] = 'Arial'
-plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['axes.linewidth'] = 0.5
-plt.rcParams['xtick.major.width'] = 0.5
-plt.rcParams['xtick.minor.width'] = 0.3
-plt.rcParams['ytick.major.width'] = 0.5
-plt.rcParams['ytick.minor.width'] = 0.3
-fonts = {"font.size":7, "axes.labelsize":7, "ytick.labelsize":7, "xtick.labelsize":7}
-plt.rcParams.update(fonts)
 
-
+#Simple function that reads abf files and returns data as a tuple containing data (list) and sampling rate (int)
 def read_abf(file_loc):
     a = AxonRawIO(file_loc)
     a.parse_header()
@@ -32,33 +22,30 @@ def read_abf(file_loc):
     data = a.get_analogsignal_chunk() * a.header["signal_channels"][0][5]
     return data[:, 0], sr
 
-
 def meta():
     meta = pd.read_excel(meta_loc)
-
     for i, x in meta.iterrows():
         fn = x["name"] + " " + str(x.cell) + "-" + str(x.ind)
         fn = fn + ".ABF"
         meta.loc[i, "filename"] = fn
     return meta
 
+#Analyzes complex spikes based on the list of files in meta
 def complex_spikes():
-    cs_meta = meta()
-    os.chdir(data_loc)
+    cs_meta = meta() #reads meta
+    os.chdir(data_loc) #changes working directory to location of data
     for i, x in cs_meta.iterrows():
-        data, sr = read_abf(x.filename)
-        window = np.arange(0, len(data), sr)
-        control_df = pd.DataFrame()
-        Area = []
+        data, sr = read_abf(x.filename) #gets the data
+        window = np.arange(0, len(data), sr) #determines the window of 1 sweep
+        control_df = pd.DataFrame() #output frame
+        Area = [] #list for temporary storage
         for start in window:
-            baseline = np.min(data[int(0.3 * sr)+start:int(0.4 * sr)+start])
-            cs_data = data[int(0.20 * sr)+start:int(0.24 * sr)+start]
-            cs_data = cs_data- baseline
-            x_data = np.arange(0,40,0.02)
-            
-            area = trapz(cs_data, x_data)
+            baseline = np.min(data[int(0.3 * sr)+start:int(0.4 * sr)+start]) #determines the baseline holding voltage
+            cs_data = data[int(0.20 * sr)+start:int(0.24 * sr)+start] #gets the data of the complex spike
+            cs_data = cs_data- baseline #normalizes everything to be at V = 0
+            x_data = np.arange(0,40,0.02) #x_data based on the length of the complex spike (in this case 40 ms)
+            area = trapz(cs_data, x_data) #determines the area of the complex spike from cs_data and x_data
             Area.append(area)
-
             # plt.plot(x_data, cs_data, color='black')
             # plt.fill_between(x_data, cs_data,alpha=0.25)
             # plt.savefig(os.path.join(output_loc, "summary_" + str(x.name) + ".png"))
@@ -67,27 +54,7 @@ def complex_spikes():
         cs_meta.loc[i,'area'] = np.mean(Area)
     return cs_meta
 
-
-def check_cells():
-    os.chdir(data_loc)
-    data, sr = read_abf('stijn_141221 1-157.ABF')
-    window = np.arange(0, len(data), sr)
-    Area = []
-    for start in window:
-        baseline = np.min(data[int(0.3 * sr)+start:int(0.4 * sr)+start])
-        cs_data = data[int(0.20 * sr)+start:int(0.25 * sr)+start]
-        cs_data = cs_data- baseline
-        x_data = np.arange(0,10,0.02)
-        
-        area = trapz(cs_data, x_data)
-        Area.append(area)
-
-    plt.plot(x_data, cs_data)
-    plt.fill_between(x_data, cs_data,alpha=0.25)
-    plt.savefig('cs_test.png', dpi=1000)
-    #plt.close()
-    return Area
-
+#plots a single cell's complex spike
 def cs_plot():
     os.chdir(data_loc)
     data, sr = read_abf('stijn_141221 1-157.ABF')
@@ -96,43 +63,37 @@ def cs_plot():
     for start in window:
         baseline = np.min(data[int(0.3 * sr)+start:int(0.4 * sr)+start])
         cs_data = data[int(0.05 * sr)+start:int(0.30 * sr)+start]
-
-        
-
     plt.plot(cs_data)
-
     plt.savefig('cs_test.png', dpi=1000, color='black')
-    #plt.close()
     return Area
 
+#plots the area of all cells based on pos/neg
 def plot_area(cs_meta):
-    #cs_meta = complex_spikes()
     cm= 1/2.54
     fig, ax = plt.subplots(figsize=(3.4*cm,3))
     sns.boxplot(x='group',y='area', data=cs_meta, fliersize=0, ax=ax, linewidth=0.5)
     sns.swarmplot(x='group',y='area', data=cs_meta, ax=ax, palette=["#000000","#FFFFFF"], edgecolor='black', linewidth=0.5, size=3, 
                   marker = '^')
-    
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.set_ylim(0,500)
     ax.set_xticklabels(['+','-'])
     ax.set_xlabel('EAAT4')
     ax.set_ylabel('Area (mV x ms)')
-
     fig.tight_layout()
     fig.savefig(os.path.join(output_loc, "complex_spikedata.pdf"))
     return fig, ax
 
+#plots a single cs, for figures
 def stim(cs_meta):
     os.chdir(data_loc)
     data, sr = read_abf('stijn_141221 1-157.ABF')
     fig, ax = plt.subplots(dpi=300)
     stim_data = data[int(0.09*sr):int(0.26*sr)]
-        
     ax.plot(stim_data,color='black')
     return
 
+#A really bloated function that does statistics for complex spike area. Not my best work.
 def stats(cs_meta):
     meta = {}
     vars = ['area']
@@ -147,7 +108,6 @@ def stats(cs_meta):
         pos_sd = tstd(pos_data.loc[:,v])
         pos_sh = shapiro(pos_data.loc[:,v])
         neg_sh = shapiro(neg_data.loc[:,v])
-    
         meta['pos_mean'] = pos_mean
         meta['neg_mean'] = neg_mean
         meta['pos_sem'] = pos_sem
@@ -165,10 +125,10 @@ def stats(cs_meta):
     stat_df.to_excel(os.path.join(output_loc, 'LTD ' + str(v) + '.xlsx'))
     return stat_df
 
-
+#Variable definitions for plotting
 if __name__ == "__main__":
     plt.style.use('seaborn-paper')
-    color = ["#5BE12D","#EC008C"] #Original green/red
+    color = ["#5BE12D","#EC008C"] 
     sns.set_palette(sns.color_palette(color), desat=1)
     plt.rcParams['font.sans-serif'] = 'Arial'
     plt.rcParams['font.family'] = 'sans-serif'
